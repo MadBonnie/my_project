8a2e91f0a46fb52272bed630b360f7da0617920c
cesbafiz <cesar.bafiz@gmail.com>
2024-12-18 11:28:55 -0600

    Segundo intento, edición de la página

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # my_project
-#First my_project on TT - 
+#First my_project on TT - creamos una aplicación web destinada para trazar gráficos respecto a una base de datos. En este caso la base de datos es sobre información de vehículos.
---
+++ app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import streamlit as st
+import plotly_express as px
+import time
+
+
+st.header('Proyecto - César Barrios')
+
+welcome_msg = """
+Este es mi primer proyecto en TripleTen.
+Soy César Barrios, futuro analista de datos. 
+No es mucho, pero es trabajo honesto. 
+:)
+"""
+
+
+def stream_data():
+    for word in welcome_msg.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+car_data = pd.read_csv('vehicles_us.csv')
+
+hist_button = st.button('Construir histograma')
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')    
+    fig = px.histogram(car_data, x="odometer") 
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Construir gráfico de dispersión')
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
